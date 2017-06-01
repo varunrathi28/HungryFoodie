7ef1912ff210096c714eaa5d98c21545ec552a57
Varun Rathi <varunrathi28@gmail.com>
2017-06-01 12:32:14 +0530

    Project added

---
+++ HungryFoodie/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HungryFoodie
+//
+//  Created by Varun Rathi on 01/06/17.
+//  Copyright © 2017 vrat28. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
